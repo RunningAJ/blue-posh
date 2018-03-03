9d12985db9e76e762be14132c314d77b5f6d5c7b
Adam Johnston <oifveteran06@gmail.com>
2018-03-03 17:46:22 -0500

    Add files via upload

---
+++ decode_base64.ps1
@@ -0,0 +1,27 @@
+﻿function decode-base64 {
+<#
+
+.SYNOPSIS
+This function will help you decode a base64 string. This users ASCII be default to decode the characters.
+
+.DESCRIPTION
+
+ ____  ____  ___  __  ____  ____     ____   __   ____  ____  ___   ___ 
+(    \(  __)/ __)/  \(    \(  __)___(  _ \ / _\ / ___)(  __)/ __) / _ \
+ ) D ( ) _)( (__(  O )) D ( ) _)(___)) _ (/    \\___ \ ) _)(  _ \(__  (
+(____/(____)\___)\__/(____/(____)   (____/\_/\_/(____/(____)\___/  (__/
+
+.EXAMPLE
+decode-base64 -base64 aGVsbG8=
+
+.NOTES
+All commands must containing spaces musted be enclosed in quotes. 
+ 
+#>
+    Param( 
+    [Parameter(Mandatory=$true)][ValidateNotNull()][String] $BASE64
+    )
+# Command that is executed to decode base64
+$results = [system.text.encoding]::ASCII.GetString([System.Convert]::FromBase64String($base64))
+return $results
+}
---
+++ decode_hex.ps1
@@ -0,0 +1,38 @@
+﻿function decode-hex {
+<#
+.SYNOPSIS
+This function will help you decode a HEX string
+
+.DESCRIPTION
+
+64 65 63 6F 64 65 2D 68 65 78
+64 65 63 6F 64 65 2D 68 65 78
+64 65 63 6F 64 65 2D 68 65 78
+64 65 63 6F 64 65 2D 68 65 78
+64 65 63 6F 64 65 2D 68 65 78
+64 65 63 6F 64 65 2D 68 65 78
+
+See the example below for proper usage
+
+.EXAMPLE
+decode-hex -hex "68656c6c6f"
+
+decode-hex -hex "68 65 6c 6c 6f"
+
+decode-hex -hex "x68x65x6cx6cx6f"
+
+
+ 
+#>
+    Param( 
+    [Parameter(Mandatory=$true)][ValidateNotNull()][String] $hex
+    )
+# Command that is executed to decode HEX
+if ( $hex -cmatch ' ') { $hex.Split(" ") | forEach {[char]([convert]::toint16($_,16))} | forEach {$result = $result + $_} 
+return $result
+} ELSEIF ( $hex -cmatch 'x' ) {
+$hex = ($hex -replace 'x','') -split '(..)' | Where-Object { $_ -ne "$null" } | forEach {[char]([convert]::toint16($_,16))} | forEach {$result = $result + $_}
+return $result
+} ELSE { $hex = ($hex -replace 'x','') -split '(..)' | Where-Object { $_ -ne "$null" } | forEach {[char]([convert]::toint16($_,16))} | forEach {$result = $result + $_}
+return $result }
+}
